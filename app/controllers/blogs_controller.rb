a43292aeba8d30ced0e065e6c590e5dbb7f74237
Takako Sato <tea-ko@live.jp>
2018-08-16 11:59:19 +0000

    お気に入り機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,6 +16,8 @@ class BlogsController < ApplicationController
   
   def create
     @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
+    
     if @blog.save
       redirect_to blogs_path, notice: "つぶやきました！"
     else
@@ -38,11 +40,13 @@ class BlogsController < ApplicationController
   
   def confirm
     @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
     render :new if @blog.invalid?
   end 
   
   def show
     @blog = Blog.find(params[:id])
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
   end
   
   private
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,8 @@
 class Blog < ApplicationRecord
-  validates :content, length: { in: 1..140 }
+    validates :content, length: { in: 1..140 }
+    
+    belongs_to :user
+    
+    has_many :favorites, dependent: :destroy
+    has_many :favorite_users, through: :favorites, source: :user
 end
